3c98f619e39f87a439a7e40b62032059de8fdb8e
Andrea <andrea.f.matera@gmail.com>
2025-08-30 18:31:44 +0200

    Added change password icon

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,7 +5,8 @@ import {
     FaHistory,
     FaInbox,
     FaFutbol,
-    FaLock
+    FaLock,
+    FaKey
 } from 'react-icons/fa';
 
 export default function NavBar() {
@@ -14,7 +15,8 @@ export default function NavBar() {
         { icon: FaUsers, label: "Scambia localmente", link: "/local" }, // Meglio di FaLayerGroup per gruppi
         { icon: FaInbox, label: "Scambi in arrivo", link: "/incoming" }, // Inbox per le richieste
         { icon: FaFutbol, label: "Squadra", link: "/team" }, // Pallone da calcio per la squadra
-        { icon: FaLock, label: "Admin", link: "/admin/" } // Gestione squadre
+        { icon: FaLock, label: "Admin", link: "/admin/" }, // Gestione squadre
+        { icon: FaKey, label: "Cambio password", link: "/change-pwd" } //
     ];
 
     return (
